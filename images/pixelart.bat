7e2825b5ec946ea9bbc727ef7885b0a9e4366cfd
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2026-03-19 14:46:19 +0100

    testing a new bat and I dont want to lose any change

--- images/pixelart.bat
+++ images/pixelart.bat
@@ -24,20 +24,16 @@ echo ==========================================
 echo.
 echo   1. Generate Palete
 echo   2. Generate Image
-echo   3. Generate CSV
-echo   4. Copy pre-done images
-echo   5. Generate CSV -2
-echo   6. Exit
+echo   3. Generate Output file
+echo   4. Exit
 echo.
 echo ==========================================
-set /p CHOICE="  Select an option (1-5): "
+set /p CHOICE="  Select an option (1-4): "
 
 if "%CHOICE%"=="1" goto FUNCTION_ONE
 if "%CHOICE%"=="2" goto FUNCTION_TWO
 if "%CHOICE%"=="3" goto FUNCTION_THREE
-if "%CHOICE%"=="4" goto FUNCTION_FOUR
-if "%CHOICE%"=="5" goto FUNCTION_FIVE
-if "%CHOICE%"=="6" goto EXIT
+if "%CHOICE%"=="4" goto EXIT
 
 echo.
 echo   [!] Invalid option. Please try again.
@@ -89,6 +85,9 @@ echo ==========================================
 echo              Generate images
 echo ==========================================
 
+rem "remove result file"
+del "%OUTPUT_DIR%\sprites.txt"
+
 for /d %%D in ("%INPUT_DIR%\*") do (
     set "FOLDER_NAME=%%~nxD"
     
@@ -104,19 +103,11 @@ for /d %%D in ("%INPUT_DIR%\*") do (
     echo Input !FOLDER_IN!
     echo Output !FOLDER_OUT!
 
-    if not exist !FOLDER_OUT! (
-        mkdir !FOLDER_OUT!
-    )
-
     for %%F in (!FOLDER_IN!\*.png) do (
         echo Processing %%~nxF
 
-        magick "%%F" ^
-            -alpha remove -alpha off ^
-            -filter point -resize !WIDTH!x!HEIGHT!! ^
-            -filter point -resize %PIXEL_W%x%PIXEL_H%! ^
-            -dither none -remap "%PALETE_FILE%" ^
-            "!FOLDER_OUT!\%%~nxF"
+        echo Generando CSV para %%~nF...
+        python image_to_palette_png.py "!FOLDER_IN!\%%~nxF" "%PALETE_FILE%" "%OUTPUT_DIR%\%%~nxF"
     )
 )
 
@@ -126,7 +117,7 @@ goto MAIN_MENU
 :FUNCTION_THREE
 cls
 echo ==========================================
-echo              Generate csv
+echo              Generate output file
 echo ==========================================
 
 rem "remove result file"
@@ -151,81 +142,7 @@ for /d %%D in ("%INPUT_DIR%\*") do (
         echo Processing %%~nxF
 
         echo Generando CSV para %%~nF...
-        python _csv_gen.py !WIDTH! !HEIGHT!  "!FOLDER_OUT!\%%~nxF" "%OUTPUT_DIR%\sprites.txt" "%PALETE_FILE%" %%~nF
-
-    )
-)
-
-pause
-goto MAIN_MENU
-
-:FUNCTION_FOUR
-cls
-echo ==========================================
-echo              Copy pre generated images
-echo ==========================================
-
-for /d %%D in ("%INPUT_DIR%\*") do (
-    set "FOLDER_NAME=%%~nxD"
-    
-    :: Split "WxH" into WIDTH and HEIGHT
-    for /f "tokens=1,2 delims=x" %%A in ("!FOLDER_NAME!") do (
-        set "WIDTH=%%A"
-        set "HEIGHT=%%B"
-    )
-    
-    echo Processing: !FOLDER_NAME! ^(WIDTH=!WIDTH! HEIGHT=!HEIGHT!^)
-    set "FOLDER_IN=%INPUT_DIR%\!FOLDER_NAME!"
-    set "FOLDER_OUT=%OUTPUT_DIR%\!FOLDER_NAME!"
-    echo Input !FOLDER_IN!
-    echo Output !FOLDER_OUT!
-
-    if not exist !FOLDER_OUT! (
-        mkdir !FOLDER_OUT!
-    )
-
-    for %%F in (!FOLDER_IN!\*.png) do (
-        echo Processing %%~nxF
-
-        magick "%%F" ^
-            -alpha remove -alpha off ^
-            -dither none -remap "%PALETE_FILE%" ^
-            "!FOLDER_OUT!\%%~nxF"
-    )
-)
-
-pause
-goto MAIN_MENU
-
-:FUNCTION_FIVE
-cls
-echo ==========================================
-echo              Generate csv -2(now its personal)
-echo ==========================================
-
-rem "remove result file"
-del "%OUTPUT_DIR%\sprites.txt"
-
-for /d %%D in ("%INPUT_DIR%\*") do (
-    set "FOLDER_NAME=%%~nxD"
-    
-    :: Split "WxH" into WIDTH and HEIGHT
-    for /f "tokens=1,2 delims=x" %%A in ("!FOLDER_NAME!") do (
-        set "WIDTH=%%A"
-        set "HEIGHT=%%B"
-    )
-    
-    echo Processing: !FOLDER_NAME! ^(WIDTH=!WIDTH! HEIGHT=!HEIGHT!^)
-    set "FOLDER_IN=%INPUT_DIR%\!FOLDER_NAME!"
-    set "FOLDER_OUT=%OUTPUT_DIR%\!FOLDER_NAME!"
-    echo Input !FOLDER_IN!
-    echo Output !FOLDER_OUT!
-
-    for %%F in (!FOLDER_IN!\*.png) do (
-        echo Processing %%~nxF
-
-        echo Generando CSV para %%~nF...
-        python image_to_palette_magick.py "!FOLDER_IN!\%%~nxF" "%PALETE_FILE%" "%OUTPUT_DIR%\sprites.txt"
+        python image_to_palette_magick.py "!FOLDER_OUT!\%%~nxF" "%PALETE_FILE%" "%OUTPUT_DIR%\sprites.txt"
     )
 )
 
