0299ba256ef6b3654bc632bdaeeb2ada9a0003ae
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2026-04-18 00:18:49 +0200

    change scores

--- src/config/ScoreConfiguration.h
+++ src/config/ScoreConfiguration.h
@@ -1,4 +1,4 @@
 #pragma once
 constexpr int SCORE_PER_KILL = 50;
-constexpr int SCORE_PER_PLAYER_HIT = 10;
-constexpr int SCORE_PER_FINISH_LEVEL = 100;
+constexpr int SCORE_PER_PLAYER_HIT = 30;
+constexpr int SCORE_PER_FINISH_LEVEL = 80;
