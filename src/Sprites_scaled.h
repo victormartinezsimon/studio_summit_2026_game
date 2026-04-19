a23f0f2075fa905cbeb65d36e7a0c26d5068b437
PC-VICTOR\Victor <victormartinezsimon@gmail.com>
2026-04-20 00:05:25 +0200

    add a custom image for big bullets, as x2 is too big

--- src/Sprites_scaled.h
+++ src/Sprites_scaled.h
@@ -5,10 +5,6 @@ constexpr unsigned int BULLET_SMALL_WIDTH = BULLET_WIDTH/2;
 constexpr unsigned int BULLET_SMALL_HEIGHT = BULLET_HEIGHT/2;
 static uint8_t sprite_bullet_small[BULLET_SMALL_WIDTH * BULLET_SMALL_HEIGHT];
 
-constexpr unsigned int BULLET_BIG_WIDTH = BULLET_WIDTH*2;
-constexpr unsigned int BULLET_BIG_HEIGHT = BULLET_HEIGHT*2;
-static uint8_t sprite_bullet_big[BULLET_BIG_WIDTH * BULLET_BIG_HEIGHT];
-
 constexpr unsigned int NUMBERS_BIG_WIDTH = NUMBERS_WIDTH*2;
 constexpr unsigned int NUMBERS_BIG_HEIGHT = NUMBERS_HEIGHT*2;
 static uint8_t sprite_numbers_big[NUMBERS_BIG_WIDTH * NUMBERS_BIG_HEIGHT];
